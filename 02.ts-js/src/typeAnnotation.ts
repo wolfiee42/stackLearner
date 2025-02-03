9d91e855c9ea8112ffb84268b47ce4a2c1fe0f52
wolfiee42 <saifalislam2022@gmail.com>
2025-02-03 21:23:36 +0600

    practicinging type parameter and type return value

---
+++ 02.ts-js/src/typeAnnotation.ts
@@ -0,0 +1,29 @@
+// typed parameter
+function greetings(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+console.log(greetings("Alice")); // Hello, Alice!
+// console.log(greetings(42)); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+
+// typed return value
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const sum = add(10, 5); // 15
+
+function logMessage(message: string): void {
+    console.log(message);
+}
+
+// optional parameters
+function displayInfo(name: string, age?: number): string {
+    return age
+        ? `${name} is ${age} years old.`
+        : `${name}'s age is unknown.`
+}
+
+console.log(displayInfo('Bob', 30)); // Bob is 30 years old.
+console.log(displayInfo('Alice')); // Alice's age is unknown
