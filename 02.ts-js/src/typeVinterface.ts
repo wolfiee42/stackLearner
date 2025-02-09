0e6a2864b887c4a485d9ea743a2ae126646a965b
wolfiee42 <saifalislam2022@gmail.com>
2025-02-09 21:23:50 +0600

    extending type and interface

--- 02.ts-js/src/typeVinterface.ts
+++ 02.ts-js/src/typeVinterface.ts
@@ -6,8 +6,32 @@ type User1 = {
 }
 
 // Interaface
-interface User2 {
+interface IUser2 {
     id: number;
     name: string;
     email: string;
+}
+
+// interface extend
+interface IAdmin2 extends IUser2 {
+    role: string;
+}
+
+const admin2: IAdmin2 = {
+    id: 2,
+    name: 'saif',
+    email: 'saif@xyz.com',
+    role: 'admin'
+}
+
+// type extend
+type TAdmin1 = User1 & {
+    role: string;
+}
+
+const admin1: TAdmin1 = {
+    id: 1,
+    name: "Ashik",
+    email: 'ashik@esa.com',
+    role: "super-admin"
 }
