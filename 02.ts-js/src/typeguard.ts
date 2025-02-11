4bacc5186a0b7bba947236e8e2b6c5dadc1320bd
wolfiee42 <saifalislam2022@gmail.com>
2025-02-11 23:01:14 +0600

    learning parsing data and union functionalities in ts

--- 02.ts-js/src/typeguard.ts
+++ 02.ts-js/src/typeguard.ts
@@ -61,7 +61,7 @@ async function fetchUser(url: string) {
 }
 
 
-// usecase 02
+// usecase 02 // Handling Event Types
 function isMouseEvent(event: Event) {
     return 'clientX' in event && 'clientY' in event;
 }
@@ -73,3 +73,57 @@ document.addEventListener('click', (event) => {
         console.log('Non-mouse event triggered.');
     }
 })
+
+// usecase 03 // Union Types in Functions
+type Cat = {
+    meow: () => void;
+}
+type Dog = { bark: () => void };
+
+type Animal = Cat | Dog;
+
+function isCat(animal: Animal): animal is Cat {
+    return (animal as Cat).meow !== undefined;
+}
+
+function makeSound(animal: Cat | Dog) {
+    if (isCat(animal)) {
+        animal.meow();
+    } else {
+        animal.bark();
+    }
+}
+
+
+// usecase 04 // Parsing and Validating Complex Data
+type Order = {
+    id: number;
+    total: number;
+    items: [];
+}
+
+function isOrder(order: unknown): order is Order {
+    return (
+        typeof order === 'object' &&
+        order !== null &&
+        'id' in order !== null &&
+        typeof (order as any).id === 'number' &&
+        'total' in order !== null &&
+        typeof (order as any).total === 'number' &&
+        Array.isArray((order as any).items)
+    )
+}
+
+function processOrder32(json: string) {
+    const data: unknown = JSON.parse(json);
+
+    if (isOrder(data)) {
+        console.log(`id number is ${data.id} and total is ${data.total}`);
+    } else {
+        console.log('Invalid Order data');
+
+    }
+}
+
+const validOrderJSON = '{"id": "123", "total": 50.5, "items": ["item1", "item2"]}';
+processOrder32(validOrderJSON);
