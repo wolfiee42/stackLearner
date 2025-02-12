c4f7eb17c76749b34d130dc69e392536061e1e3f
wolfiee42 <saifalislam2022@gmail.com>
2025-02-12 22:45:44 +0600

    learning all the utility funciton

--- 02.ts-js/src/utilityTypes.ts
+++ 02.ts-js/src/utilityTypes.ts
@@ -92,3 +92,67 @@ const user44: SafeUser = {
     name: 'amin',
     email: 'amin@ws.com'
 }
+
+// Record<Keys, Type>
+
+type UserRole33 = 'admin' | 'user' | 'guest';
+
+const userPermission: Record<UserRole33, string[]> = {
+    admin: ['read', 'write', 'delete'],
+    user: ['read', 'write'],
+    guest: ['read']
+}
+
+// Exclude<Type, ExcludedUnion>
+type UserRole34 = 'admin' | 'user' | 'guest';
+
+type ActiveUser34 = Exclude<UserRole34, 'guest'>;
+
+const activeUser: ActiveUser34 = 'admin';
+
+
+// Extract<Type, Union>
+type UserRole35 = 'admin' | 'user' | 'guest';
+
+type DeactivatedUser = Extract<UserRole35, 'admin' | 'user'>;
+
+const deactivatedUser: DeactivatedUser = 'user'
+
+
+// NonNullable<Type>
+type UserInput = string | null | undefined;
+
+type ValidInput = NonNullable<UserInput>;
+
+function validProcess(input: ValidInput) {
+    console.log(`Valid input : ${input}`);
+}
+
+// validProcess(null) // Error
+validProcess('hello')
+
+// Promise<Type>
+function fetchData44(): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('hello man'), 1000);
+    })
+}
+
+const results: Promise<string> = fetchData44();
+
+// Awaited<Type>
+
+type fetchData45 = Promise<{
+    id: 1,
+    name: 'ashik'
+}>
+
+type ResolvedData = Awaited<fetchData45>;
+
+function fetch455(): Promise<ResolvedData> {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve({ id: 1, name: 'ashik' }), 1000);
+    })
+}
+
+fetch455().then((data) => console.log(data.name));
